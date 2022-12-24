836ed473bb26ebe6dd3986862a177fbd3669ac34
sayed-salah14 <121361360+sayed-salah14@users.noreply.github.com>
2022-12-24 23:16:32 +0200

    Create day_1.py

---
+++ day_1.py
@@ -0,0 +1,63 @@
+print("Sayed Salah")
+print("I love PHP")
+print(True)
+
+# print(true) error t must be  capitial letter
+# Print Error P must be  small letter
+
+#***************************************#
+                #Variables
+#***************************************#
+# 1sa = "sayed" error
+# in = "sayed" error
+# for = "sayed" error
+#print ="sayed" error
+
+sa = "sayed"
+
+print("Hello",sa,"How Are you doing")
+print("Hello"+sa+"How Are you doing man?") #a what diffrent between + and , in concatenate
+
+#***************************************#
+                #dataTypes
+#***************************************#
+x = "Sayed's String" #String
+y = 10 #integer
+h = 10.20 #Float
+Z = True #Boolen
+ZZ = False #Boolen
+#**************///////*******************#
+m1 ="sayed \
+    salah\n"
+m2 ="\nSayed Salah" 
+m3= "Hello Mr \"Sayed\""   
+print("\nThis is my name: ",m1)   
+print("My name is: "+m2,"\n",m3,"\n")
+
+#******************************************#
+m5= """
+s1
+s2
+s3
+s4
+s5
+\n"""
+print(m5)
+#*******************************************#
+m6 ="Ahmed Samir"
+m7= "AHMED SAMIR"
+m8= "ahmed samir sayed"
+print(m6[0],m6[-5],"\n",m6[6:11],"\n") # -1 -2 strarts from the end ******** [index,index+1]
+
+#*******************************************#
+print(m6.upper(),"\n") # var.function
+print(m7.lower(),"\n")
+print(m7.lower(),"\n")
+print(m8.capitalize(),"cap")
+print(m8.title())
+#*******************************************#
+
+print(m7.islower())
+print(m7.isupper())
+#*******************************************#
+
